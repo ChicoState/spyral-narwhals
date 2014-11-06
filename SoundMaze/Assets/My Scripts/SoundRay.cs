b0e0e1b509e33114375bfe646694d73a43bad1e2
jnicholson3 <jnicholson3@mail.csuchico.edu>
2014-11-06 01:29:52 -0800

    Add phase 1 of unity game
    Basic level design and player movement

---
+++ SoundMaze/Assets/My Scripts/SoundRay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundRay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
